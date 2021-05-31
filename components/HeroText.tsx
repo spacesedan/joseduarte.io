f74e6e62b0a1439c81f82b41efd71cda71cef6cf
Jose Duarte <joseduarte@joses-air.home>
2021-05-31 00:05:43 -0700

    testing something out

--- components/HeroText.tsx
+++ components/HeroText.tsx
@@ -2,12 +2,12 @@ export default function HeroText() {
   return (
     <div className='py-5 md:py-0 '>
       <h1 className='hero-text'> Jose Duarte</h1>
-      <p className='hero-text_subtext'>
+      <h2 className='hero-text_subtext'>
         {' '}
         <span className='md:inline-block block'>Front-End Developer,</span>
         <span className='md:inline-block block'>UX/UI Designer</span>
         <span className='md:inline-block block'>3D Enthusiast</span>
-      </p>
+      </h2>
     </div>
   );
 }
