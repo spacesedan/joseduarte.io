c1156df8e6b8b3753ae5efcbc825e21ef064605b
Jose Duarte <joseduarte@joses-air.home>
2021-05-31 22:22:54 -0700

    added footer

--- styles/components/Footer.tsx
+++ styles/components/Footer.tsx
@@ -2,32 +2,34 @@ import { PORTFOLIO_REPO, LINKEDIN } from 'lib/constants';
 
 export default function Footer() {
   return (
-    <footer className='col-span-full h-56 flex justify-center items-center '>
-      <div className='grid grid-cols-3 w-full grid-rows-2 md:text-xl text-lg text-gray-900 dark:text-gray-600 '>
-        <div className='col-start-1 col-end-1 flex justify-center items-center row-span-1 '>
-          <a
-            href={PORTFOLIO_REPO}
-            target='_blank'
-            rel='noreferrer'
-            className='hover:text-green-400'
-          >
-            Portfolio Repo
-          </a>
+    <footer className='footer'>
+      <div className='w-full'>
+        <div className='footer--grid'>
+          <div className='footer--grid__row-1-col-1 '>
+            <a
+              href={PORTFOLIO_REPO}
+              target='_blank'
+              rel='noreferrer'
+              className='hover:text-green-400'
+            >
+              Portfolio Repo
+            </a>
+          </div>
+          <div className='footer--grid__row-1-col-2 '>
+            <a
+              href={LINKEDIN}
+              target='_blank'
+              rel='noreferrer'
+              className='hover:text-green-400 '
+            >
+              LinkedIn
+            </a>
+          </div>
+          <div className='footer--grid__row-1-col-3'></div>
+          <p className='row-start-2 row-span-1 col-span-full mt-16 '>
+            &#169; Jose Duarte 2021
+          </p>
         </div>
-        <div className='col-start-2 col-end-2 flex justify-center items-center row-span-1 '>
-          <a
-            href={LINKEDIN}
-            target='_blank'
-            rel='noreferrer'
-            className='hover:text-green-400 '
-          >
-            LinkedIn
-          </a>
-        </div>
-        <div className='col-start-3 col-end-3 flex justify-center items-center row-span-1'></div>
-        <p className='row-start-2 row-span-1 col-span-full mt-16 '>
-          &#169; Jose Duarte 2021
-        </p>
       </div>
     </footer>
   );
